5c14086f93a9b74875d2ff4d5d49dcffd15697ad
PWWWWWWWW <94442349+PWWWWWWWW@users.noreply.github.com>
2026-04-10 15:17:38 +0800

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # constraints: len(n + 1), element:[1, n]
+        # purpose: find dupli, with others all appears one time
+        
+        # pseudo
+        # [1, 3, 8, 3, 2]
+        # brute force sol
+        from collections import defaultdict
+
+        count = defaultdict(int)
+        for num in nums:
+            count[num] += 1
+
+        for num, freq in count.items():
+            if freq > 1:
+                return num
+
+        return o
+        
