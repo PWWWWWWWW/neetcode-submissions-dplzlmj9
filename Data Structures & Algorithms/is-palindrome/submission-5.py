698776f3f883f6a8fcfe25ca833a51f3c16f5ea1
PWWWWWWWW <94442349+PWWWWWWWW@users.noreply.github.com>
2026-04-10 14:10:34 +0800

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_s = ''
+        filter_char = []
+
+        for char in s:
+            if char.isalnum():        
+                filter_char.append(char.lower())
+        
+        new_s = new_s.join(filter_char)
+
+        left = 0
+        right = len(new_s) - 1
+
+        while left <= right:
+            if new_s[left] != new_s[right]:
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
